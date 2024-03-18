ca8ce005686883a0e201db6ff5705e7aef762a56
wangkehan <shjyh651079151@gmail.com>
2024-03-18 16:07:01 +0800

    config knex pool min 0

--- src/dao/db.ts
+++ src/dao/db.ts
@@ -4,7 +4,11 @@ const db = knex({
     client: "pg",
     connection: import.meta.env.DB_CONNECTION_URL,
     searchPath: ["public"],
-    debug: process.env.NODE_ENV === "development"
+    debug: process.env.NODE_ENV === "development",
+    pool: {
+        min: 0,
+        max: 10
+    }
 })
 
 export default db;
