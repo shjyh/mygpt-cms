764c2f4df8d75229c663f454c52588803b751e1f
wangkehan <shjyh651079151@gmail.com>
2024-06-18 16:03:14 +0800

    upgrade astro framework

--- src/pages/x/_Card.tsx
+++ src/pages/x/_Card.tsx
@@ -3,7 +3,7 @@ import { Pagination } from "swiper/modules";
 
 import 'swiper/css';
 import 'swiper/css/pagination';
-import './style.css';
+import './_style.css';
 
 import { useState } from "react";
 
